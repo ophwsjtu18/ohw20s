a27c835949fb46344398afd9c40e9d4557bb04cd
wmh010810 <62879930+wmh010810@users.noreply.github.com>
2020-04-20 22:57:19 +0800

    Add files via upload

---
+++ students/wmh/w3/dog_class.py
@@ -0,0 +1,20 @@
+class Dog(object):
+    """模拟小狗"""
+    def __init__(self,name,age):
+        """初始化属性"""
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """模拟小狗被命令时蹲下"""
+        print(self.name.title() + " is now sitting.")
+
+    def roll_over(self):
+        """模拟小狗被命令时打滚"""
+        print(self.name.title() + " rolled over!")
+my_dog=Dog("willie",6)
+
+print("My dog's name is " + my_dog.name.title() + ".")
+print("My dog is " + str(my_dog.age) + " years old.")
+my_dog.sit()
+my_dog.roll_over()

50ecc6ec3a88808c5e6cff19d3f8b7faf0eb31b4
wmh010810 <62879930+wmh010810@users.noreply.github.com>
2020-04-22 22:02:12 +0800

    Update socket_build.py

--- students/wmh/w3/socket_build.py
+++ students/wmh/w3/socket_build.py
@@ -1,13 +1,20 @@
-from mcpi.minecraft import Minecraft
 import socket
 
-mc=Minecraft.create()
-print("connected")
-pos=mc.player.getTilePos()
+s=socket.socket()
+s.connect(("127.0.0.1",4711))
+msg="player.getTile()\n"
+s.send(msg.encode("ascii"))
+pos=s.recv(1024)
+pos1=str(pos)
+s0=pos1.split('\'')
+s1=s0[1].split('\\n')
+s2=s1[0].split(',')
+x=float(s2[0])
+y=float(s2[1])
+z=float(s2[2])
 for i in range(3):
     for j in range(3):
-        s=socket.socket()
-        s.connect(("127.0.0.1",4711))
-        msg="world.setBlock("+str(pos.x+i-1)+","+str(pos.y)+","+str(pos.z+j-1)+",41)"
+        msg="world.setBlock("+str(x+i-1)+","+str(y)+","+str(z+j-1)+",41)\n"
         s.send(msg.encode("ascii"))
-        s.close()
+s.close()
+

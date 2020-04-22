459497dbf4a985be74a99e25c080bf55a0e6d3d0
yuanyuanroseupeha <62286933+yuanyuanroseupeha@users.noreply.github.com>
2020-04-22 23:27:33 +0800

    Create get.py

---
+++ students/yyy/Week 6/web_moveplayer/get.py
@@ -0,0 +1,20 @@
+#!C:\Users\dell\AppData\Local\Programs\Python\Python37\python.exe
+#coding=utf-8
+import cgi,cgitb
+form=cgi.FieldStorage()
+mc_move=form.getvalue('mc_move')
+mc_dir=form.getvalue('mc_dir')
+#map=[[0,5,0,5]for x in range(10)]
+#pring(mapprint("Content-type:text/html")
+print()
+print("<html>")
+print("<head>")
+print("<meta charset=\"utf-8\">")
+print("<title>moocxing</title>")
+print("</head>")
+print("<body>")
+print("<h2>%s:%s</b2>" % (mc_move,mc_dir))
+print("</body>")
+print("</html>")
+with open('../htdocs/'+mc_move+".txt",'w') as f:
+       f.write(mc_dir)

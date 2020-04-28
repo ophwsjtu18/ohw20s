f304d53054639fde380ee6944448f16a20a24815
yutianchen666 <62009606+yutianchen666@users.noreply.github.com>
2020-04-28 23:56:25 +0800

    Create gethouse.py

---
+++ students/ytc/homework-week8/mc_sethouse_html/gethouse.py
@@ -0,0 +1,31 @@
+#!C:\Users\85316\AppData\Local\Programs\Python\Python38\python.exe
+#coding=utf-8
+import cgi, cgitb
+
+form = cgi.FieldStorage()
+
+house = form.getvalue("house")
+housex = form.getvalue("housex")
+housey = form.getvalue("housey")
+housez = form.getvalue("housez")
+#map=[[0.5,0.5]for x in range (10)]
+#print (map)
+print ("Content-type:text/html")
+print ()
+print ("<html>")
+print ("<head>")
+print ("<meta charset=\"utf-8\">")
+print ("<title>build house</title>")
+print ("</head>")
+print ("<body>")
+print ("<h2>(%s,%s,%s)</h2>" % (housex,housey,housez))
+print ("</body>")
+print ("</html>")
+with open('../htdocs/house.txt','w') as f:
+    f.write(f'{house}')
+with open('../htdocs/x.txt','w') as f:
+    f.write(f'{housex}')
+with open('../htdocs/y.txt','w') as f:
+    f.write(f'{housey}')
+with open('../htdocs/z.txt','w') as f:
+    f.write(f'{housez}')

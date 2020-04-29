c3d64781ae2fc7e26b844aa4007dc3187399b9d4
Roy-ce <62175616+Roy-ce@users.noreply.github.com>
2020-04-29 11:56:06 +0800

    Create controlend.py

---
+++ students/gzz/ex4/ex4.1/controlend.py
@@ -0,0 +1,21 @@
+
+import os
+import time
+from mcpi.minecraft import  Minecraft
+mc=Minecraft.create()
+while True:
+	pos=mc.player.getTilePos()
+	f=open("mc_move.txt")
+	a=f.read()
+	if a=='w':
+		mc.player.setTilePos(pos.x,pos.y,pos.z+1)
+		time.sleep(1)
+	elif a=='s':
+		mc.player.setTilePos(pos.x,pos.y,pos.z-1)
+		time.sleep(1)
+	elif a=="a":
+		mc.player.setTilePos(pos.x-1,pos.y,pos.z)
+		time.sleep(1)
+	else:
+		mc.player.setTilePos(pos.x+1,pos.y,pos.z)
+		time.sleep(1) 

db04d798f5e6deeb5aace5d03a813380580feb8d
wmh010810 <62879930+wmh010810@users.noreply.github.com>
2020-05-08 13:07:32 +0800

    Delete arduinocase.bat

--- students/wmh/homeworks/arduinocase.bat
+++
@@ -1,9 +0,0 @@
-rem delete any previous 
-del arduinocase.binvox
-
-rem use default resolution of 256 and downsample twice, so resolution is 64
-rem you will to add your own arguments here - see http://www.patrickmin.com/minecraft/
-binvox arduinocase.stl -down -down -down
-
-rem now display it
-viewvox arduinocase.binvox

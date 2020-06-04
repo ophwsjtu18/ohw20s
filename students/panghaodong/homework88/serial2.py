855e7a19d0cb1c97a71f1b4a45f1021e7cac904b
PHD12345 <46048274+PHD12345@users.noreply.github.com>
2020-06-04 16:12:55 +0800

    Create serial2.py

---
+++ students/panghaodong/homework88/serial2.py
@@ -0,0 +1,11 @@
+import serial
+
+ser=serial.Serial("COM4")
+ser.write("100,200,100\n".encode())
+
+while True:
+    c=input("please input bottom arm and top arm speed")
+    print(c)
+    if c=="q":
+        break
+    ser.write(c.encode())

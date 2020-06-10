d215f58911287bebde96e9e0862625e64dad8dd5
MARKJH16 <62985263+MARKJH16@users.noreply.github.com>
2020-06-10 11:17:57 +0800

    Create mc_box_binvox.py

---
+++ students/hjh/20200506/mc_box_binvox.py
@@ -0,0 +1,81 @@
+import binvox_rw
+
+from mcpi.minecraft import Minecraft
+
+import mcpi.block as block
+
+import os
+
+
+
+mc  = Minecraft.create()
+
+pos = mc.player.getTilePos()
+
+
+
+name = str(input('Enter the name of the stl file:'))
+
+
+
+# Without any robustness
+
+binvox_name = name.replace('.stl', '.binvox', 1)
+
+
+
+if os.path.exists(binvox_name):
+
+  os.remove(binvox_name)
+
+
+
+os.system('.\\binvox.exe -d 50 ' + name)
+
+
+
+with open(binvox_name, 'rb') as f:
+
+  model = binvox_rw.read_as_3d_array(f)
+
+print(model.dims)
+
+print(model.scale)
+
+print(model.translate)
+
+# print(model.data)
+
+
+
+for y in range(model.dims[1]):
+
+  print("layer y = ",y)
+
+  layer_data = model.data[y]
+
+  # stringlayer = ""
+
+  for x in range(model.dims[0]):
+
+    # stringlayer = stringlayer + "\n"
+
+    for z in range(model.dims[2]):
+
+      if model.data[x][y][z] == True:
+
+        # stringlayer = stringlayer + '1'
+
+        mc.setBlock(pos.x + x, pos.y + y, pos.z + z,
+
+					block.STONE.id)
+
+      else:
+
+        # stringlayer = stringlayer + '0'
+
+        mc.setBlock(pos.x + x, pos.y + y, pos.z + z,
+
+					block.AIR.id)
+
+  # print(stringlayer)

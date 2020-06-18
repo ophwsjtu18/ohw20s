26183042c3d9b95901e5463da1dd8d0f979170f3
sjtu19djq <62992664+sjtu19djq@users.noreply.github.com>
2020-06-18 12:15:58 +0800

    Add files via upload

---
+++ students/djq/wk3.py
@@ -0,0 +1,43 @@
+import socket
+
+
+
+s = socket.socket()
+
+s.connect(('localhost', 4711))
+
+s.send('player.getPos()\n'.encode('ascii'))
+
+temp = s.recv(1024)
+
+temps = str(temp)
+
+pos1 = temps.split('\'')
+
+pos2 = pos1[1].split('\\n')
+
+pos = pos2[0].split(',')
+
+x, y, z = float(pos[0]), float(pos[1]), float(pos[2])
+
+for i in range(3):
+
+  for j in range(3):
+
+    tmp = 'world.setBlock('
+
+    tmp += str(x - 1 + i)
+
+    tmp += ','
+
+    tmp += str(y - 1)
+
+    tmp += ','
+
+    tmp += str(z - 1 + j)
+
+    tmp += ',41)\n'
+
+    s.send(tmp.encode('ascii'))
+
+s.close()
